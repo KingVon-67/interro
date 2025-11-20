88712e3adc59fedc71bd15689d9c88e1f1fe3f3a
Isiah Mineur <isiah.mineur@cesjb.be>
2025-11-20 15:58:26 +0100

    3 users créés

--- Main.js
+++ Main.js
@@ -1,2 +1,10 @@
 import Etudiant from "./Etudiant";
 
+var user1 = new Etudiant("isiah",17,"info")
+
+var user2 = new Etudiant("lusseni",16,"info")
+
+var user3 = new Etudiant("sacha",18,"latin")
+
+
+
